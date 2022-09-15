07b91c6d1a60566030c02618329225848d7113ed
gurpreetsingh-hub <a21gursinsin@inspedralbes.cat>
2022-09-15 11:00:24 +0200

    0

--- i.php
+++ i.php
@@ -1,10 +1,22 @@
-<?php
-      if(file_exists('Quiz.json'))
+
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doument</title>
+  </head>
+  <body>
+    <?php
+      if(file_exists('quiz.json'))
       {
-        $filename = 'Quiz.json';
+        $filename = 'quiz.json';
         $data = file_get_contents($filename); 
         $users = json_decode($data);
-        echo ('hola ');
-      }else{echo ( 'hola mundo');};
-      var_dump($users[2]);
-  ?>
+      }
+    ?>
+    <?php $pregunta = ($users[11]->question) ?>
+    <p><?php echo ($pregunta) ?></p>
+  </body>
+  </html>
