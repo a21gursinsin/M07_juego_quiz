2aa17822556ee9eddb7a585368344729ca0b112d
a21gursinsin <a21gursinsin@inspedralbes.cat>
2022-09-27 21:12:11 +0200

    empezar de 0

--- getPreguntes.php
+++ getPreguntes.php
@@ -1,20 +1,28 @@
 <?php
-    $Npreguntas = $_GET["Npreguntas"];
-    echo $Npreguntas;
-    function alterarPreguntas($Npreguntas) {
-      $listPreguntas=array();
-      for ($i = 0; $i <= $Npreguntas; $i++) {
-        $n=rand(0, 11); 
-        if(in_array($n, $listPreguntas)){
-          $i--;
-        }else {
-          array_push($listPreguntas,$n);
-        }
-      }
-      return $listPreguntas;
+$Npreguntas = $_GET["Npreguntas"];
+function alterarPreguntas($Npreguntas)
+{
+  $listPreguntas = array();
+  for ($i = 0; $i <= $Npreguntas; $i++) {
+    $n = rand(0, 11);
+    if (in_array($n, $listPreguntas)) {
+      $i--;
+    } else {
+      array_push($listPreguntas, $n);
     }
-    $listPreguntas=alterarPreguntas($Npreguntas);
-  ?>
+  }
+  return $listPreguntas;
+}
+$listPreguntas = alterarPreguntas($Npreguntas);
+
+?>
+<script>
+  fetch('http://localhost/gur/M07_JUEGO_QUIZ/Quiz.json')
+    .then(response => response.json())
+    .then(data => datos = data);
+</script>
 
 
-  <script>window.location="question.php";</script>
+<script>
+  window.location = "question.php";
+</script>
